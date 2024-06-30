55a203b8fe0913160e434df90c5090fbdb455377
Tomoe-12 <li8993han@gmail.com>
2024-06-30 13:45:33 +0630

    boolean

---
+++ boolean.py
@@ -0,0 +1,21 @@
+hungry = True
+loveMe = False
+
+# print(hungry)
+# print(loveMe)
+
+
+# comparison operator > < >= <= == 
+
+# print(5>3)
+
+# print(5==5)
+# print('5'==5)
+# print(5>=4)
+
+# logical operator (and or not )
+print(not False )
+
+print( 5>1 and 5>10)
+
+print( 5>1 or 5>3)
