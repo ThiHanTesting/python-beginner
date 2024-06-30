8bca5a0871e11711f687eccc99cd0b834e79402b
Tomoe-12 <li8993han@gmail.com>
2024-06-30 15:00:58 +0630

    for loop

---
+++ ConditionsAndLooping/forLoop.py
@@ -0,0 +1,15 @@
+
+# fruits =['apple','orange','watermelon','mango']
+
+# for fruit in fruits : 
+#     print(f'{fruit} is fruit')
+
+names = ['aung aung','kyaw kyaw','aye aye','su su']
+
+for name in names : 
+  if name == 'kyaw kyaw' :
+       print(f'{name} is instructor')
+       break
+  else : 
+       print(f'{name} is student')
+
