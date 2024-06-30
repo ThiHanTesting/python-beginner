4f31cc34247f1c43242a15baf22c27558576c7d0
Tomoe-12 <li8993han@gmail.com>
2024-06-30 13:27:44 +0630

    first commit

---
+++ input.py
@@ -0,0 +1,14 @@
+#  print hello to console
+print('hello')
+
+name = input('name : ')
+age = input ('age : ')
+print('the name is '+name+"age is "+age)
+
+
+
+# area calculator (pi r**2)
+PI = 3.14 
+r=int(input('radius : '))
+area = PI*r**2 
+print('area is ', area)
