d14f42720b047d35d83f1377fb3183829480d00f
Tomoe-12 <li8993han@gmail.com>
2024-06-30 15:15:36 +0630

    range usuage with for loop

---
+++ ConditionsAndLooping/ranges.py
@@ -0,0 +1,13 @@
+# nums = [0,1,2,3,4]
+
+# for num in range(5) : 
+#     print(num)
+
+# for num in range(2,30,4) : 
+#     print(num)
+
+pizzas = ['chessy','spicy','mayo chicken','cock pizza']
+
+# for pizza in pizzas : print(pizza)
+
+for num in range(len(pizzas)) : print(pizzas[num])
