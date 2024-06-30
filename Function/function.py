69d443e25ec4e9d12a32b1a51c0f3f81adbc5569
Tomoe-12 <li8993han@gmail.com>
2024-07-01 01:29:38 +0630

    function start

---
+++ Function/function.py
@@ -0,0 +1,7 @@
+def greet (user='aye aye',time='aftermoon') : 
+    print(f'good {time} , {user}  ')
+
+greet('khun thi han','morning')
+greet('daw hnin hnin yee','evening')
+greet()
+greet(time='evening',user='thi ha')
