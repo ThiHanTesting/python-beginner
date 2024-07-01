462a9a37315dc2335c330f1dbb85f5a759fd35a7
Tomoe-12 <li8993han@gmail.com>
2024-07-02 01:18:51 +0630

    dictionary

---
+++ Function/dictionary.py
@@ -0,0 +1,34 @@
+person = {
+    'name' : 'khun thi han',
+    'age' : 20
+}
+
+# person['hair_color']='black'
+# print(person)
+
+
+# in key usage
+# print('name' in person)
+
+# if 'nasdsme' in person : 
+#     print('work')
+
+# person_keys = list(person.keys())
+# print(person_keys)
+# person_values = list(person.values())
+# print(person_values)
+
+# update 
+# person.update({'name' : 'thi thi'})
+# print(person)
+
+# delete
+# person.pop('name')
+# person.popitem()
+# del person['name']
+# del person
+# print(person)
+
+# looop
+# for x in person :
+#     print(x)
