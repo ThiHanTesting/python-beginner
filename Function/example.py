0ec16627b760f5f2f491c0f2466f57b5a0ab567f
Tomoe-12 <li8993han@gmail.com>
2024-07-03 01:56:52 +0630

    sorted set  ( usuage )

--- Function/example.py
+++ Function/example.py
@@ -4,7 +4,8 @@ while True :
     name = input('name : ')
     age = input('age  :')
 
-    person[name]=age
+    person[name]= age
+
 
     another = input('another ? y/n')
     if another == 'y' : 
@@ -13,5 +14,15 @@ while True :
         break
 
 
-for (key,value) in person.items() : 
-    print(f' key : {key} value : {value}')
+# for (key,value) in person.items() : 
+#     print(f' key : {key} value : {value}')
+
+print(person)
+ages = list(person.values())
+
+for age in set(ages) : 
+    count = ages.count(age)
+    print(f'{age} years old -- ',count )
+
+    # 20 years old - 2
+    # 40 years old - 1
---
+++ Function/sorted_set.py
@@ -0,0 +1,15 @@
+nums = [2,42,4,32,6,6,2,5,145,]
+# print(sorted(nums))
+
+names = ['hlaing min than','Kyaw kyaw','aung aung','aung la']
+# print(sorted(names))
+
+
+# set 
+# print(set(nums))
+# for num in set(nums) : 
+#     print(num)
+
+ages = [20,40,50,20,30,40]
+for age in set(ages) :
+    print(age)
