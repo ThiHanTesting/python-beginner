cf6fd012af3d753513b470221026a8b892ba9f3a
Tomoe-12 <li8993han@gmail.com>
2024-07-03 14:14:05 +0630

    class level attribute and method

--- classConcept(OOP)/classFromScratch.py
+++ classConcept(OOP)/classFromScratch.py
@@ -1,4 +1,5 @@
 class Car : 
+    sterringWheel = 1   #class level attribute
     def __init__(self,name='lol',wheels='4') : 
         self.name = name
         self.wheels = wheels
@@ -6,9 +7,21 @@ class Car :
     def drive(self) : 
         print(f'{self.name} with {self.wheels} is driving')
 
-Lambo = Car('Toyota',6)
-print(Lambo.name)
-Lambo.drive()
+    @classmethod
+    def common(cls) : 
+        print(f'all car is only {cls.sterringWheel} sterring wheel .')
 
-marcedes = Car('Marcedes',6)
-marcedes.drive()
+
+# Lambo = Car('Toyota',6)
+# print(Lambo.name)
+# Lambo.drive()
+
+# marcedes = Car('Marcedes',6)
+# marcedes.drive()
+
+# class level attribute and method
+print(Car.sterringWheel)
+Car.common()
+
+Lamborr = Car()
+Lamborr.common()
