eb3520ce468075821a3255a3489a16f2ab070a5b
Tomoe-12 <li8993han@gmail.com>
2024-07-04 01:48:50 +0630

    comprehension way

---
+++ advanceTrickAndCombiningConcepts/comprehensionWay.py
@@ -0,0 +1,9 @@
+# prices =  [2000,1000,3000,5000]
+
+# double_prices = [price*2 for price in prices]
+# print(double_prices)
+
+nums = [1,2,3,4,5,6,7,8,9,10]
+
+even_double_nums = [num*2 for num in nums if (num%2) == 0]
+print(even_double_nums)
